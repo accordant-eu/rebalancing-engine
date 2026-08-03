5557dda6d88a0aabc6339c2d5f8a190e044ccb29
Johan Hellman <johan.o.hellman@gmail.com>
2026-08-03 07:16:06 +0200

    chore: migrate from ts-jest to @swc/jest for TS 7 support

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+/** @type {import('jest').Config} */
 module.exports = {
-  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
   testEnvironment: 'node',
   rootDir: '.',
   roots: ['<rootDir>/src/', '<rootDir>/tests/'],
